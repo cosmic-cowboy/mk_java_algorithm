a27a7ddc892708b865e44d0200ab22abcab5c358
hiroo takaku <slgerkamp@gmail.com>
2014-06-21 18:45:11 +0900

    基本的なデータ構造：今日で今年が何日終わったか

--- src/main/java/com/slgerkamp/mkjavaalgorithm/basicdatastructure/BasicDataStructure.java
+++ src/main/java/com/slgerkamp/mkjavaalgorithm/basicdatastructure/BasicDataStructure.java
@@ -8,6 +8,36 @@ import java.util.List;
  */
 public class BasicDataStructure {
 
+	
+	/**
+	 * 今日で今年が何日終わったかを求める
+	 * @param year
+	 * @param month
+	 * @param day
+	 * @return
+	 */
+	public static int dayInYear(int year, int month, int day){
+
+		int [][] mdays = {
+			{31,28,31,30,31,30,31,31,30,31,30,31},
+			{31,29,31,30,31,30,31,31,30,31,30,31}
+		};
+		int yearNum = 0;
+		// 閏年判定
+		// 4で割り切れる年 かつ 100で割り切れない、もしくは400で割り切れる
+		if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0 ){
+			yearNum = 1;
+		}
+		
+		int resultDay = 0;
+		// 日付の計測
+		for(int i = 0; i < month-1; i++){
+			resultDay += mdays[yearNum][i];
+		}
+		resultDay += day;
+		System.out.println(resultDay);
+		return resultDay;
+	}
 	/**
 	 * 素数を求める
 	 * @param number
