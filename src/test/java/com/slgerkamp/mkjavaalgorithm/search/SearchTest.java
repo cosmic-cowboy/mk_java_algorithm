4f8be1b662eaa21101842d247126e903199544e0
hiroo takaku <slgerkamp@gmail.com>
2014-06-21 19:41:51 +0900

    探索：２分探索テスト

---
+++ src/test/java/com/slgerkamp/mkjavaalgorithm/search/SearchTest.java
@@ -0,0 +1,45 @@
+package com.slgerkamp.mkjavaalgorithm.search;
+
+import org.junit.runner.RunWith;
+import org.junit.experimental.theories.*;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+@RunWith(Theories.class)
+public class SearchTest {
+
+	@DataPoints
+	public static Fixture [] PARAM_1 = {
+		new Fixture(new int[]{ 1, 3, 4, 5, 6, 7, 8 },  4, 2),
+		new Fixture(new int[]{ 1, 3, 4, 5, 6, 7 },  4, 2),
+		new Fixture(new int[]{ 1, 3, 4, 5, 6},  4, 2),
+		new Fixture(new int[]{ 1, 3, 4},  4, 2),
+		new Fixture(new int[]{ 1, 3},  4, -1),
+		new Fixture(new int[]{ 5, 7},  4, -1)
+	};
+	
+	@Theory
+	public void 探索_２分探索(Fixture f){
+		System.out.println(f);
+		assertThat(Search.binarySearch(f.intArray, f.searchNumber), is(f.expected));
+	}
+
+	static class Fixture {
+		int [] intArray;
+		int searchNumber;
+		int expected;
+		
+		public Fixture( int[] intArray, int searchNumber, int expected) {
+			super();
+			this.intArray = intArray;
+			this.searchNumber = searchNumber;
+			this.expected = expected;
+		}
+		
+		@Override
+		public String toString() {
+			return String.format("when searchNumber =%s, intArray =%s, expected=%s", searchNumber,intArray, expected);
+		}
+	}
+}
