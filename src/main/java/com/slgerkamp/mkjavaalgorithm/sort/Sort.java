d11e16b680bfca30c4295f4182768e1e372d5724
hiroo takaku <slgerkamp@gmail.com>
2014-07-05 17:49:29 +0900

    ソート：単純交換ソート（バブルソート）：走査を限定

--- src/main/java/com/slgerkamp/mkjavaalgorithm/sort/Sort.java
+++ src/main/java/com/slgerkamp/mkjavaalgorithm/sort/Sort.java
@@ -11,6 +11,59 @@ public class Sort {
 	 */
 	public static enum ORDER {ASCENDING, DESCENDING}
 
+	/**
+	 * 単純交換ソート(バブルソート) <br>
+	 * 改良版<br>
+	 * 
+	 * @param targetArray ソートされていないランダムな配列
+	 * @param order 並び順
+	 * @return ソートされたランダムな配列
+	 */
+	public static SortResult bubbleSort_version3(int [] targetArray, ORDER order){
+		
+		// ソート比較回数
+		int counter = 0;
+		// 配列の末尾
+		int lastElementNum = targetArray.length - 1;
+		// 配列の末尾からどこまでを比較するかを決定
+		int k = 0;
+		while(k < lastElementNum){
+
+			// n番目に小さい値をソートした際の交換回数
+			// 配列の末尾から順番に並んだ２つの値を取得し
+			// 後方の配列の値が前方の配列の値より小さい場合
+			// ２つの配列の順番を交換する
+			// それを配列数 - 1 すると最小値が配列の先頭にくる
+			// 次に配列の中で２番目に小さい値を求める
+			int last = lastElementNum;
+			for(int j = lastElementNum; j > k; j--){
+				// ソート比較回数
+				counter++;
+				int old_num = targetArray[j];
+				int new_num = targetArray[j - 1];
+				// shallow copyだから配列の値の受け渡しができているけど、
+				// ちょっと嫌だな
+				switch(order){
+					case ASCENDING:
+						if(ascendingOrder(targetArray, j, old_num, new_num)){
+							last = j;
+						};
+						break;
+					case DESCENDING:
+						if(descendingOrder(targetArray, j, old_num, new_num)){
+							last = j;
+						}
+						break;
+					default :
+						break;
+				}
+			}
+
+			k = last;
+		}
+		return new SortResult(targetArray, counter);
+	}
+
 	
 	/**
 	 * 単純交換ソート(バブルソート) <br>
